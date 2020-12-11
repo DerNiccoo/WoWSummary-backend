1ea2fb636fbe0b6438ceba2c538a4ec5d5c38641
Debian <debian@weni1034.novalocal>
2020-12-11 16:10:45 +0000

    Added first end points to call from frontend

--- app/models.py
+++ app/models.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 from sqlalchemy import or_
 from app import db
+from dataclasses import dataclass
 
+@dataclass
 class Guild(db.Model):
-  guild_id = db.Column(db.Integer, primary_key=True)
-  realm = db.Column(db.String(32))
-  name = db.Column(db.String(32), index=True)
-  faction = db.Column(db.String(16))
-  last_modified = db.Column(db.DateTime)
+  guild_id : int = db.Column(db.Integer, primary_key=True)
+  realm: str = db.Column(db.String(32))
+  name: str = db.Column(db.String(32), index=True)
+  faction: str = db.Column(db.String(16))
+  last_modified: int = db.Column(db.Integer)
   player = db.relationship("Guild_player", back_populates="guild")
 
   def __init__(self, guild_id, realm, name, faction, last_modified):
@@ -23,13 +25,53 @@ class GuildQuery(object):
     guild = Guild.query.filter(Guild.guild_id == guild_id).first()
     return guild.last_modified if hasattr(guild, 'last_modified') else 0
 
+  @staticmethod
+  def get_guild(realm, name):
+    guild = Guild.query.filter(Guild.realm.ilike(realm), Guild.name.ilike(name)).first()
+    return guild
+
+  @staticmethod
+  def update_guild(guild_new):
+    guild = Guild.query.filter(Guild.guild_id == guild_new.guild_id).first()
+    guild.realm = guild_new.realm
+    guild.faction = guild_new.faction
+    guild.last_modified = guild_new.last_modified
+    db.session.commit()
+    return True
+
+@dataclass
 class Guild_player(db.Model):
-  player_id = db.Column(db.Integer, primary_key=True)
-  guild_id = db.Column(db.Integer, db.ForeignKey('guild.guild_id'), primary_key=True)
+  player_id: int = db.Column(db.Integer, primary_key=True)
+  guild_id: int = db.Column(db.Integer, db.ForeignKey('guild.guild_id'), primary_key=True)
   guild = db.relationship("Guild", back_populates="player", foreign_keys=[guild_id])
-  name = db.Column(db.String(32), index=True)
-  playable_class = db.Column(db.String(32))
-  race = db.Column(db.String(32))
-  rank = db.Column(db.Integer)
-  last_modified = db.Column(db.DateTime)
-  character_id = db.Column(db.Integer)
+  name: str = db.Column(db.String(32), index=True)
+  level: int = db.Column(db.Integer)
+  playable_class: str = db.Column(db.String(32))
+  race: str = db.Column(db.String(32))
+  rank: int = db.Column(db.Integer)
+  last_modified: int = db.Column(db.Integer)
+
+  def __init__(self, player_id, guild_id, name, level, playable_class, race, rank, last_modified):
+    self.player_id = player_id
+    self.guild_id = guild_id
+    self.name = name
+    self.level = level
+    self.playable_class = playable_class
+    self.race = race
+    self.rank = rank
+    self.last_modified = last_modified
+
+class Guild_playerQuery(object):
+  @staticmethod
+  def get_last_modified(player_id):
+    player = Guild_player.query.filter(Guild_player.player_id == player_id).first()
+    return player.last_modified if hasattr(player, 'last_modified') else 0
+
+  @staticmethod
+  def get_all_guild_player(realm, name):
+    character = Guild_player.query\
+      .join(Guild, Guild.guild_id == Guild_player.guild_id)\
+      .filter(Guild.name.ilike(name))\
+      .filter(Guild.realm.ilike(realm))\
+      .all()
+    return character
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from app import db
-from app.models import Guild, Guild_player
+from app.models import Guild, GuildQuery, Guild_player, Guild_playerQuery
 from flask import Flask, render_template, redirect, request, jsonify, make_response
 
 import requests
@@ -8,14 +8,50 @@ from requests.auth import HTTPBasicAuth
 from datetime import datetime
 import datetime
 
+classes = ['warrior', 'paladin', 'hunter', 'rogue', 'prist', 'death-knight', 'shamane', 'mage', 'warlock', 'monk', 'druid', 'demon-hunter']
+refresh_interval = 5 * 60
+
+@app.route("/guild/<string:realm>/<string:guild>")
+def get_guild(realm, guild):
+  guild = GuildQuery.get_guild(realm, guild)
+
+  if guild == None:
+    return make_response('Guild not found', 404)
+  else:
+    return make_response(jsonify(guild), 200)
+
+
 @app.route("/guild/<string:realm>/<string:guild>/roster")
 def get_player_from_guild(realm, guild):
+  character = Guild_playerQuery.get_all_guild_player(realm, guild)
+  
+  if character == None:
+    return make_response('No Character found', 404)
+  else:
+    return make_response(jsonify(character), 200)
 
-  body = 'grant_type=client_credentials'
-  header = {'Content-Type': 'application/x-www-form-urlencoded',}
-  response = requests.post('https://eu.battle.net/oauth/token', data=body, headers=header, auth=HTTPBasicAuth(app.config['BLIZZ_CLIENT_ID'], app.config['BLIZZ_CLIENT_SECRET']))
 
-  return make_response(jsonify(action = response.text), 200)
+def refresh_guild(guild_data, last_modified, recorded_modified):
+  if recorded_modified == 0: # INSERT INTO
+    guild = Guild(guild_data['id'], guild_data['realm'], guild_data['name'], guild_data['faction'], last_modified)
+    db.session.add(guild)
+    db.session.commit()
+  elif recorded_modified + refresh_interval < last_modified: # UPDATE
+    guild = Guild(guild_data['id'], guild_data['realm'], guild_data['name'], guild_data['faction'], last_modified)
+    GuildQuery.update_guild(guild)
+
+def refresh_guild_roster(guild_roster, guild_id, last_modified, recorded_modified):
+  if recorded_modified > last_modified:
+    return True
+  
+  for char in guild_roster:
+    if recorded_modified == 0: # INSERT INTO
+      char = Guild_player(char['id'], guild_id, char['name'], char['level'], char['_class'], char['race'], char['rank'], last_modified)
+      db.session.add(char)
+    elif recorded_modified + refresh_interval < last_modified:
+      print("asd")
+
+  db.session.commit()
 
 @app.route("/refresh")
 def refresh_everything():
@@ -26,7 +62,11 @@ def refresh_everything():
   access_token = response.json()['access_token']
 
   guild_roster, guild_roster_lm = update_guild_roster('blackrock', 'shockwave', access_token)
-  print(guild_roster_lm.timestamp())
+  last_modified = GuildQuery.get_last_modified(guild_roster['guild']['id'])
+
+  refresh_guild(guild_roster['guild'], guild_roster_lm, last_modified)
+  refresh_guild_roster(guild_roster['character'], guild_roster['guild']['id'], guild_roster_lm, 0)#last_modified)
+
   # Schauen ob Gilde bereits vorhanden, wenn ja:
   # Vergleichen, ob LM größer als der in DB ist
   # Wenn ja: Updaten! (Gilden Meta infos)
@@ -37,9 +77,31 @@ def refresh_everything():
   return make_response(guild_roster, 200)
 
 def convert_datetime(date_time_str):
-  return datetime.datetime.strptime(date_time_str, '%a, %d %b %Y %H:%M:%S %Z')
+  return int(datetime.datetime.strptime(date_time_str, '%a, %d %b %Y %H:%M:%S %Z').timestamp())
 
 def update_guild_roster(realm, name, token):
-  response = requests.get(f'https://eu.api.blizzard.com/data/wow/guild/{realm}/{name}/roster?namespace=profile-eu&locale=de_EU&access_token={token}')
-  return (response.json(), convert_datetime(response.headers['Date']))
-  
+  response = requests.get(f'https://eu.api.blizzard.com/data/wow/guild/{realm}/{name}/roster?namespace=profile-eu&locale=de_DE&access_token={token}')
+
+  res = response.json()
+
+  data = { 'guild': {'name': '', 'id': '', 'realm': '' , 'faction': ''}, 'character': '' }
+  data['guild']['name'] = res['guild']['name']
+  data['guild']['id'] = res['guild']['id']
+  data['guild']['realm'] = res['guild']['realm']['name']
+  data['guild']['faction'] = res['guild']['faction']['name']
+
+  chars = []
+  for member in res['members']:
+    char = {'id': '', 'leve': '', 'name': '', '_class': '', 'realm': '', 'rank': '', 'race': ''}
+    char['id'] = member['character']['id']
+    char['level'] = member['character']['level']
+    char['name'] = member['character']['name']
+    char['_class'] = classes[member['character']['playable_class']['id'] - 1]
+    char['realm'] = member['character']['realm']['slug']
+    char['rank'] = member['rank']
+    char['race'] = member['character']['playable_race']['id']
+    chars.append(char)
+
+  data['character'] = chars
+
+  return (data, convert_datetime(response.headers['Date']))
