28054bdb35d060e7579015b3a958e76369e297ea
DerNiccoo <Nico.Wellermann@live.de>
2020-12-11 09:11:22 +0000

    added last modified attr

--- app/models.py
+++ app/models.py
@@ -7,7 +7,21 @@ class Guild(db.Model):
   realm = db.Column(db.String(32))
   name = db.Column(db.String(32), index=True)
   faction = db.Column(db.String(16))
+  last_modified = db.Column(db.DateTime)
   player = db.relationship("Guild_player", back_populates="guild")
+
+  def __init__(self, guild_id, realm, name, faction, last_modified):
+    self.guild_id = guild_id
+    self.realm = realm
+    self.name = name
+    self.faction = faction
+    self.last_modified = last_modified
+
+class GuildQuery(object):
+  @staticmethod
+  def get_last_modified(guild_id):
+    guild = Guild.query.filter(Guild.guild_id == guild_id).first()
+    return guild.last_modified if hasattr(guild, 'last_modified') else 0
 
 class Guild_player(db.Model):
   player_id = db.Column(db.Integer, primary_key=True)
@@ -17,4 +31,5 @@ class Guild_player(db.Model):
   playable_class = db.Column(db.String(32))
   race = db.Column(db.String(32))
   rank = db.Column(db.Integer)
+  last_modified = db.Column(db.DateTime)
   character_id = db.Column(db.Integer)
--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, render_template, redirect, request, jsonify, make_respo
 
 import requests
 from requests.auth import HTTPBasicAuth
+from datetime import datetime
+import datetime
 
 @app.route("/guild/<string:realm>/<string:guild>/roster")
 def get_player_from_guild(realm, guild):
@@ -14,3 +16,30 @@ def get_player_from_guild(realm, guild):
   response = requests.post('https://eu.battle.net/oauth/token', data=body, headers=header, auth=HTTPBasicAuth(app.config['BLIZZ_CLIENT_ID'], app.config['BLIZZ_CLIENT_SECRET']))
 
   return make_response(jsonify(action = response.text), 200)
+
+@app.route("/refresh")
+def refresh_everything():
+  body = 'grant_type=client_credentials'
+  header = {'Content-Type': 'application/x-www-form-urlencoded',}
+  response = requests.post('https://eu.battle.net/oauth/token', data=body, headers=header, auth=HTTPBasicAuth(app.config['BLIZZ_CLIENT_ID'], app.config['BLIZZ_CLIENT_SECRET']))
+  
+  access_token = response.json()['access_token']
+
+  guild_roster, guild_roster_lm = update_guild_roster('blackrock', 'shockwave', access_token)
+  print(guild_roster_lm.timestamp())
+  # Schauen ob Gilde bereits vorhanden, wenn ja:
+  # Vergleichen, ob LM größer als der in DB ist
+  # Wenn ja: Updaten! (Gilden Meta infos)
+  # Updaten Spieler: Loopen, Schauen ob ID existiert, wenn ja einfach alle roster Daten überschreiben, wenn ID nicht existiert, neu anlegen mit LM = 0 (maybe guter default wert?)
+  # 
+  # Spieler Updaten: Loopen über alle Spieler, mini call machen um LM zu erhalten, mit dem im Spieler abgleichen. Wenn neuer -> Alle Spieler Endpunkte updaten!
+
+  return make_response(guild_roster, 200)
+
+def convert_datetime(date_time_str):
+  return datetime.datetime.strptime(date_time_str, '%a, %d %b %Y %H:%M:%S %Z')
+
+def update_guild_roster(realm, name, token):
+  response = requests.get(f'https://eu.api.blizzard.com/data/wow/guild/{realm}/{name}/roster?namespace=profile-eu&locale=de_EU&access_token={token}')
+  return (response.json(), convert_datetime(response.headers['Date']))
+  
